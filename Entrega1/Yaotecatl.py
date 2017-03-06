6670fbf08bd0685dc27f98008829b065cf461afa
Javier Guajardo <javo_gc@hotmail.com>
2017-03-05 19:52:07 -0600

    Tests

--- Entrega1/Yaotecatl.py
+++ Entrega1/Yaotecatl.py
@@ -31,7 +31,7 @@ reserved = {
 
 tokens = ['LFTBRAC', 'RGTBRAC', 'LFTPAREN','RGTPAREN', 'LFTBRACSQR', 'RGTBRACSQR', 'AND', 'DOUBEQUAL',
           'NOT','OR','SEMICOLON', 'EQUAL', 'LESSTHANEQUAL', 'GREATTHANEQUAL', 'GREATTHAN', 'LESSTHAN',  
-          'PLUS', 'MINUS', 'MULTIPLICATION', 'DIVISION', 'INT', 'FLOAT', 'STRING', 'ID', 'COMMA', 'CHAR' ] + list(reserved.values())
+          'PLUS', 'MINUS', 'MULTIPLICATION', 'DIVISION', 'INT', 'FLOAT', 'STRING', 'ID', 'COMMA' ] + list(reserved.values())
 
 t_LFTBRAC = r'\{'
 t_RGTBRAC = r'\}'
@@ -56,7 +56,7 @@ t_MULTIPLICATION = r'\*'
 t_DIVISION = r'\/'
 t_INT = r'[0-9]+'
 t_FLOAT = r'[0-9]+\.[0-9]+'
-t_CHAR = r'[a-zA-Z0-9]'
+#t_CHAR = r'[a-zA-Z0-9]'
 t_STRING = r'\".*\"'
 t_ignore = ' \t'
 
@@ -87,48 +87,73 @@ import ply.yacc as yacc
 def p_program(p):
     '''program : PROGRAM ID LFTBRAC auxprogram main RGTBRAC'''
 def p_auxprogram(p):
-    '''auxprogram : vars auxprogram | function auxprogram | empty'''
+    '''auxprogram : vars auxprogram 
+    | function auxprogram 
+    | empty'''
 
 def p_array(p):
     '''array : ID LFTBRACSQR exp RGTBRACSQR'''
 
 def p_assignment(p):
-    '''assignment : assignmentaux EQUAL expression SEMICOLON | assignmentaux EQUAL call SEMICOLON'''    
+    '''assignment : assignmentaux EQUAL expression SEMICOLON 
+    | assignmentaux EQUAL call SEMICOLON'''    
 def p_assignmentaux(p):
-    '''assignmentaux : ID | array'''  
+    '''assignmentaux : ID 
+    | array'''  
 
 def p_blockreturn(p):
-    '''blockreturn : LFTBRAC blockreturnaux RGTBRAC | LFTBRAC blockreturnaux RETURN exp SEMICOLON RGTBRAC ''' 
+    '''blockreturn : LFTBRAC blockreturnaux RGTBRAC 
+    | LFTBRAC blockreturnaux RETURN exp SEMICOLON RGTBRAC ''' 
 def p_blockreturnaux(p):
-    '''blockreturnaux : statement blockreturnaux | empty ''' 
+    '''blockreturnaux : statement blockreturnaux 
+    | empty ''' 
 
 def p_block(p):
     '''block : LFTBRAC blockaux RGTBRAC'''   
 def p_blockaux(p):
-    '''blockaux : statement blockaux | empty '''     
+    '''blockaux : statement blockaux 
+    | empty '''     
 
 def p_condition(p):
-    '''condition : IF conditionaux | IF conditionaux ELSE block ''' 
+    '''condition : IF conditionaux 
+    | IF conditionaux ELSE block ''' 
 def p_conditionaux(p):
-    '''conditionaux : LFTPAREN expression RGTPAREN block ELSEIF conditionaux | LFTPAREN expression RGTPAREN block '''              
+    '''conditionaux : LFTPAREN expression RGTPAREN block ELSEIF conditionaux 
+    | LFTPAREN expression RGTPAREN block '''              
 
 def p_constant(p):
-    '''constant : ID | array | TRUE | FALSE '''
+    '''constant : ID 
+    | array 
+    | TRUE 
+    | FALSE '''
 
 def p_exp(p):
-    '''exp : term | term expaux  '''   
+    '''exp : term 
+    | term expaux  '''   
 def p_expaux(p):
-    '''expaux : PLUS exp expaux | MINUS exp expaux | empty '''    
+    '''expaux : PLUS exp expaux 
+    | MINUS exp expaux 
+    | empty '''    
 
 def p_factor(p):
-    '''factor : factoraux constant | LFTPAREN expression RGTPAREN ''' 
+    '''factor : factoraux constant 
+    | LFTPAREN expression RGTPAREN ''' 
 def p_factoraux(p):
-    '''factoraux : PLUS | MINUS ''' 
+    '''factoraux : PLUS 
+    | MINUS ''' 
 
 def p_expression(p):
-    '''expression : exp | expressionaux exp ''' 
+    '''expression : exp 
+    | expressionaux exp ''' 
 def p_expressionaux(p):
-    '''expressionaux : AND | DOUBEQUAL | NOT | OR | LESSTHANEQUAL | GREATTHANEQUAL | GREATTHAN | LESSTHAN '''                         
+    '''expressionaux : AND 
+    | DOUBEQUAL 
+    | NOT 
+    | OR 
+    | LESSTHANEQUAL 
+    | GREATTHANEQUAL 
+    | GREATTHAN 
+    | LESSTHAN '''                         
 
 def p_loop(p):
     '''loop : WHILE LFTPAREN expression RGTPAREN block ''' 
@@ -137,18 +162,32 @@ def p_write(p):
     '''write : PRINT LFTPAREN exp RGTPAREN SEMICOLON ''' 
 
 def p_parameter(p):
-    '''parameter : type ID | type ID COMMA parameter | empty  '''   
+    '''parameter : type ID 
+    | type ID COMMA parameter 
+    | empty  '''   
 
 def p_term(p):
     '''term : factor termaux  ''' 
 def p_termaux(p):
-    '''termaux : MULTIPLICATION term termaux | DIVISION term termaux | empty  ''' 
+    '''termaux : MULTIPLICATION term termaux 
+    | DIVISION term termaux 
+    | empty  ''' 
 
 def p_statement(p):
-    '''statement : assignment | condition | vars | loop | write | read | call  ''' 
+    '''statement : assignment 
+    | condition 
+    | vars 
+    | loop 
+    | write 
+    | read 
+    | call  ''' 
 
 def p_type(p):
-    '''type : INT | FLOAT | CHAR | BOOL | STRING  ''' 
+    '''type : INT 
+    | FLOAT 
+    | CHAR 
+    | BOOL 
+    | STRING  ''' 
 
 def p_main(p):
     '''main : PRIOMH block  ''' 
@@ -156,22 +195,28 @@ def p_main(p):
 def p_function(p):
     '''function : FUNCTION functionaux ID LFTPAREN functionaux2 RGTPAREN blockreturn  '''
 def p_functionaux(p):
-    '''functionaux : VOID | type  '''  
+    '''functionaux : VOID 
+    | type  '''  
 def p_functionaux2(p):
-    '''functionaux2 : parameter | empty  ''' 
+    '''functionaux2 : parameter 
+    | empty  ''' 
 
 def p_vars(p):
     '''vars : type varsaux2 SEMICOLON    ''' 
 def p_varsaux(p):
-    '''varsaux : PLUS | MINUS | empty   '''   
+    '''varsaux : PLUS 
+    | MINUS 
+    | empty   '''   
 def p_varsaux2(p):
-    '''varsaux2 : ID EQUAL varsaux constant | ID EQUAL varsaux constant COMMA varsaux2  '''
+    '''varsaux2 : ID EQUAL varsaux constant 
+    | ID EQUAL varsaux constant COMMA varsaux2  '''
 
 def p_call(p):
-    '''call : ID LFTPAREN exp RGTPAREN SEMICOLON | ID LFTPAREN exp COMMA call RGTPAREN SEMICOLON  ''' 
+    '''call : ID LFTPAREN exp RGTPAREN SEMICOLON 
+    | ID LFTPAREN exp COMMA call RGTPAREN SEMICOLON  ''' 
 
 def p_read(p):
-    '''read: READ LFTPAREN ID RGTPAREN SEMICOLON '''                              
+    '''read : READ LFTPAREN ID RGTPAREN SEMICOLON '''                              
 
 def p_empty(p):
     '''empty :'''
